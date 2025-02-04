130baa51f54a35cda73e827d0891decb52da4bcf
saurabh <saurabhjadhav2991@gmail.com>
2025-02-05 01:22:37 +0530

    3rd change

--- vpc.tf
+++ vpc.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "my_subnet" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
