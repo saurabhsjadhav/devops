6aa13972ec0ab7408cd682b53871e00e88197661
saurabh <saurabhjadhav2991@gmail.com>
2025-02-05 07:10:37 +0530

    6th change

--- vpc.tf
+++ vpc.tf
@@ -1,40 +1,51 @@
 provider "aws" {
-  region = "us-east-1as"
+  region = "us-east-1"
 }
 
-# VPC
+# Create VPC
 resource "aws_vpc" "my_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "my-vpc"
+    Name = "MyVPC"
   }
 }
 
-# Internet Gateway
+# Create Internet Gateway
 resource "aws_internet_gateway" "my_igw" {
   vpc_id = aws_vpc.my_vpc.id
 
   tags = {
-    Name = "my-igw"
+    Name = "MyInternetGateway"
   }
 }
 
-# Public Subnet (for NAT Gateway)
+# Create Public Subnet
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
 
   tags = {
-    Name = "public-subnet"
+    Name = "PublicSubnet"
   }
 }
 
-# Route Table for Public Subnet
+# Create Private Subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "PrivateSubnet"
+  }
+}
+
+# Create Route Table for Public Subnet
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.my_vpc.id
 
@@ -44,96 +55,62 @@ resource "aws_route_table" "public_rt" {
   }
 
   tags = {
-    Name = "public-rt"
+    Name = "PublicRouteTable"
   }
 }
 
-resource "aws_route_table_association" "public_rt_assoc" {
+# Associate Route Table with Public Subnet
+resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
 
-# Elastic IP for NAT Gateway (FIXED ERROR)
-resource "aws_eip" "nat_eip" {
-  domain = "vpc"
-}
-
-# NAT Gateway (Uses Correct EIP)
-resource "aws_nat_gateway" "nat_gw" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnet.id
-
-  tags = {
-    Name = "nat-gw"
-  }
-
-  depends_on = [aws_internet_gateway.my_igw]
-}
-
-# Private Subnet (for EC2 Instance)
-resource "aws_subnet" "private_subnet" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = false
-
-  tags = {
-    Name = "private-subnet"
-  }
-}
-
-# Route Table for Private Subnet (Uses NAT Gateway)
-resource "aws_route_table" "private_rt" {
+# Create Security Group
+resource "aws_security_group" "my_sg" {
   vpc_id = aws_vpc.my_vpc.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gw.id
-  }
-
-  tags = {
-    Name = "private-rt"
-  }
-}
-
-resource "aws_route_table_association" "private_rt_assoc" {
-  subnet_id      = aws_subnet.private_subnet.id
-  route_table_id = aws_route_table.private_rt.id
-}
-
-# Security Group
-resource "aws_security_group" "my_security_group" {
-  name_prefix = "my-sg"
-  vpc_id      = aws_vpc.my_vpc.id
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH from anywhere
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTP traffic
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTPS traffic
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
   }
 
   tags = {
-    Name = "my-security-group"
+    Name = "MySecurityGroup"
   }
 }
 
-# EC2 Instance in Private Subnet (Uses NAT Gateway for Internet Access)
-resource "aws_instance" "my_instance" {
-  ami                  = "ami-0cafb04477a984411"
-  key_name             = "devopskey"
-  instance_type        = "t2.micro"
-  subnet_id            = aws_subnet.private_subnet.id
-  security_groups      = [aws_security_group.my_security_group.name]
+# Launch EC2 Instance in Public Subnet
+resource "aws_instance" "my_ec2" {
+  ami             = "ami-0cafb04477a984411"
+  instance_type   = "t2.micro"
+  key_name        = "devopskey"
+  subnet_id       = aws_subnet.public_subnet.id
+  security_groups = [aws_security_group.my_sg.name]
 
   tags = {
-    Name = "my-instance"
+    Name = "Spiderman_Instance"
   }
 }
